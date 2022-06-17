8d3c9dd0c839492b231525420b87ad4897fd7eff
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-17 16:19:28 +0200

    Email Change

--- email/index.js
+++ email/index.js
@@ -1,10 +1,16 @@
 let userMail = prompt("Inserire l'email dell'utente");
 let mailComparison = ["alessiocalabrese07@gmail.com", "gigginothebest@hotmail.com", "markuscron73@gmail.com"];
 console.log(mailComparison);
+let judgement = false;
 
-let i = mailComparison.indexOf(userMail);
-if (i != -1) {
+for (let index = 0; index < mailComparison.length; index++) {
+    if (userMail === mailComparison[index]) {
+        judgement = true;
+    }
+}
+
+if (judgement === true) {
     console.log("La mail è verificata!")
 }else{
-    console.warn("La mail NON è verficata!")
+    console.log("La mail NON è verificata!")
 }
